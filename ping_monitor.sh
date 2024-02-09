8e96eef956e7e9d69a28508b6d6985deae34f6cc
mmwangi-eabx <157103136+mmwangi-eabx@users.noreply.github.com>
2024-02-09 10:16:21 +0300

    Add support for multiple ips
    Co-Authored-By: Moses (Mwangi) Ngige <moses.soft.eng@gmail.com>

--- ping_monitor.sh
+++ ping_monitor.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-# Get configurations.
-source /etc/ping_monitor.conf
+# Get configurations
+source /etc/ping_monitor.conf  
 
-# Log file path.
-log_file="/var/log/ping_monitor/ping_monitor.log"
+# Get IP addresses and intervals
+IFS=' ' read -ra IP_ARRAY <<< "$IP_ADDRESSES"
+IFS=' ' read -ra INTERVAL_ARRAY <<< "$INTERVALS"
 
-ping_ip() {
-    ip=$1 
-    log=$2
-    
-    if ping -c 1 $ip >/dev/null 2>&1; then
-        echo "$(date '+%Y-%m-%d %H:%M:%S') $(hostname) ping_monitor[$$]: INFO Ping successful to $ip" >> $log
-    else
-        echo "$(date '+%Y-%m-%d %H:%M:%S') $(hostname) ping_monitor[$$]: ERROR Ping failed to $ip" >> $log
-    fi
-}
+# Loop through each IP address and interval
+for i in "${!IP_ARRAY[@]}"; do
+  IP_ADDRESS=${IP_ARRAY[i]}
+  INTERVAL=${INTERVAL_ARRAY[i]}
 
-while true
-do
-    ping_ip $IP_ADDRESS $LOG_FILE
-    sleep $INTERVAL
+  # Run ping_ip.sh in the background with the given IP address and interval
+  sudo ./ping_ip.sh "$IP_ADDRESS" "$INTERVAL" "$LOG_FILE" &
 done
+
+# Wait for all background processes to finish
+wait
